83eb092a6e8e0d957dd13ebe4384d5bc8c3da272
Rheno001 <renfredreuben@gmail.com>
2025-12-14 23:52:05 +0100

    update

--- app/contact.jsx
+++ app/contact.jsx
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     title: {
-        fontWeight: 'bold',
+        fontWeight: 'semibold',
         fontSize: 18,
 
     },
